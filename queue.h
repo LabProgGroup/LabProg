4964d0d021608194e0398f4cdbc10d78df5d8062
Bruno Sesso <bsesso@haskell.linux.ime.usp.br>
2014-04-23 20:55:55 -0300

    Adicionei o queue.c .h mas só fiz as structs mais ou menos

---
+++ queue.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "utils.h"
+#include "enemy.h"
+
+typedef struct {
+    Node *first;
+    Node *last;
+} Queue;
+
+typedef struct {
+    Enemy *enemy;
+    Node *next;
+}   
