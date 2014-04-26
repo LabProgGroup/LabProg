b0035b19219dc52ba8605ac5c65644fe35214dcd
nijek <nikolai.kotsifas@gmail.com>
2014-04-26 13:48:06 -0300

    Não fiz o isAtScreen ainda pois não temos X, Y da tela

--- shot.c
+++ shot.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "shot.h"
 #include "utils.h"
+/*Módulo da velocidade do tiro*/
+float shotNorm;
+
 /*Recebe uma struct contendo as posições espaciais (x,y,z)
  * e as velocidades desses eixos (x,y,z) e retorna um ponteiro para
  * um tiro.
  */
-Shot* createShot(Position shotP, Position shotV, int damage) {
+Shot* createShot(Position shotP, Velocity shotV, int damage) {
     Shot *sh = (Shot*) malloc(sizeof (Shot));
     sh->shotPosition = shotP;
     sh->shotVelocity = shotV;
@@ -19,3 +22,30 @@ void freeShot(Shot* sh) {
     if (sh != NULL)
         free(sh);
 }
+/*Recebe um ponteiro para um tiro e atualiza sua posição na tela.*/
+void updateShot(Shot* sh) {
+    
+    sh->shotPosition.x = spaceTimeEquation(sh->shotPosition.x, sh->shotVelocity.x);
+    sh->shotPosition.y = spaceTimeEquation(sh->shotPosition.y, sh->shotVelocity.y);
+    sh->shotPosition.z = spaceTimeEquation(sh->shotPosition.z, sh->shotVelocity.z);
+    computeShotNorm(sh);
+}
+/*Recebe uma posição So, velocidade v e retorna a nova 
+ * posição após o tempo de atualização da tela.
+ */
+float spaceTimeEquation(float initialPosition, float v) {
+    return (initialPosition + v*clockTick);
+}
+
+/*Recebe o ponteiro para um tiro e calcula a norma de suas velocidades (x,y,z).
+ */
+float computeShotNorm(Shot* sh) {
+    shotNorm = sqrt(sh->shotVelocity.x*sh->shotVelocity.x + sh->shotVelocity.y*sh->shotVelocity.y + sh->shotVelocity.z*sh->shotVelocity.z); 
+    return shotNorm;
+}
+
+
+
+
+
+
--- utils.c
+++ utils.c
@@ -8,3 +8,13 @@
 float distance(Position a, Position b) { 
     return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y) + (a.z - b.z)*(a.z - b.z));
 }
+
+/*
+ Define o tick do relógio em segundos.
+ clockTick = 1/fps.
+*/
+float clockTick = (float) 1/20;
+
+/*Recebe a posição da nave e de algum outro objeto (tiro, inimigo) e verifica se ele ainda
+ * está na tela, retornando TRUE em case afirmativo e FALSE, caso contrário. */
+int isAtScreen(Position shipP, Position objectP);
